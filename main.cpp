48ff4de885a97b7b7e593719edd93bbb491f133d
zhenya11114 <fatality14nik@gmail.com>
2021-02-11 18:38:05 +0300

    lab1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//--------------------Подключаемые библиотеки--------------------//
 #include <headers/mpi.h>
 #include <iostream>
 #include <cstring>
@@ -5,24 +6,56 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    int ProcNum, ProcRank, RecvRank;
+    //--------------------Иициализация--------------------//
+    int procNum, procRank, recv;
+    int m = 2;//количество итераций
     MPI_Status Status;
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
-    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-    cout << ProcNum;
-    if (ProcRank == 0) {
-        // Действия, выполняемые только процессом с рангом 0
-        printf ("\n Hello from process %3d", ProcRank);
-        for (int i = 1; i < ProcNum; i++) {
-            MPI_Recv(&RecvRank, 1, MPI_INT, MPI_ANY_SOURCE,
-                     MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-            printf("\n Hello from process %3d", RecvRank);
+    MPI_Comm_size(MPI_COMM_WORLD, &procNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    //--------------------Главные процесс--------------------//
+    if (procRank == 0) {
+        printf(" Num of processors: %3d", procNum);
+        printf("\n Main process with rank: %3d", procRank);
+        int id = 0;
+
+        printf("\n Send message to process: %3d", 1);
+        //отправляем сообщение "0" процессу 1
+        MPI_Send(&id, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    }
+    //--------------------Остальные процессы--------------------//
+    else {
+        for(int i = 0; i < m; i++) {
+            printf("\n Process %3d receiving message... ", procRank);
+            printf("\n Current time is: %3d", MPI_Wtime());
+            //процесс ождидает сообщение из любого источника
+            //первый попавшийся процесс перехватит управление
+            MPI_Recv(&recv, 1, MPI_INT, MPI_ANY_SOURCE,
+            MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
+            printf("\n Process recieved massage: %3d", recv);
+            printf("\n Current time is: %3d", MPI_Wtime());
+
+            //recv+1 станет отправляемым сообщением
+            ++recv;
+            //последний процесс в цепочке
+            if(procRank == procNum-1) {
+                if(m != i+1){
+                    //отправляет сообщение recv процессу с рангом на 1 больше своего
+                    printf("\n Process sending massage: %3d", recv);
+                    printf("\n Current time is: %3d", MPI_Wtime());
+                    MPI_Send(&recv, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+                }
+            }
+            //любой другой процесс
+            else {
+                //отправляет сообщение recv процессу с рангом на 1 больше своего
+                printf("\n Process %3d sending massage: %3d", recv);
+                printf("\n Current time is: %3d", MPI_Wtime());
+                MPI_Send(&recv, 1, MPI_INT, procRank+1, 0, MPI_COMM_WORLD);
+            }
         }
     }
-    else {
-        MPI_Send(&ProcRank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    }
+    //--------------------Завершение работы MPI--------------------//
     MPI_Finalize();
     return 0;
 }
