a08ae1e5a669135ac850e1614b8f04ed7beeca4a
zhenya11114 <fatality14nik@gmail.com>
2021-02-25 21:49:11 +0300

    lab2

--- main.cpp
+++ main.cpp
@@ -2,60 +2,61 @@
 #include <headers/mpi.h>
 #include <iostream>
 #include <cstring>
+#include <chrono>
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     //--------------------Иициализация--------------------//
     int procNum, procRank, recv;
-    int m = 2;//количество итераций
-    MPI_Status Status;
+    int m=5;//кол-во итераций
+
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
-    //--------------------Главные процесс--------------------//
+
+
+    auto start = std::chrono::steady_clock::now();
+
+    //--------------------Запуск цикла--------------------//
     if (procRank == 0) {
-        printf(" Num of processors: %3d", procNum);
-        printf("\n Main process with rank: %3d", procRank);
-        int id = 0;
+        recv = 0;
 
-        printf("\n Send message to process: %3d", 1);
-        //отправляем сообщение "0" процессу 1
-        MPI_Send(&id, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        cout << "Num of processors: " << procNum << endl;
+        cout << "Main process with rank: " << procRank << endl;
+        cout << "Start broadcasting" << endl;
+        MPI_Bcast(&recv, 1, MPI_INT, procRank, MPI_COMM_WORLD);
     }
-    //--------------------Остальные процессы--------------------//
-    else {
-        for(int i = 0; i < m; i++) {
-            printf("\n Process %3d receiving message... ", procRank);
-            printf("\n Current time is: %3d", MPI_Wtime());
-            //процесс ождидает сообщение из любого источника
-            //первый попавшийся процесс перехватит управление
-            MPI_Recv(&recv, 1, MPI_INT, MPI_ANY_SOURCE,
-            MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-            printf("\n Process recieved massage: %3d", recv);
-            printf("\n Current time is: %3d", MPI_Wtime());
+    //--------------------Главный цикл--------------------//
+    while(true)
+    {
+        //Вычисляем значение root, из которого будем получать сообщение
+        int fromProc = (procNum+procRank-1) % procNum;
+        cout << "Processor " << procRank << " recv the message from root " <<  fromProc << endl;
 
-            //recv+1 станет отправляемым сообщением
-            ++recv;
-            //последний процесс в цепочке
-            if(procRank == procNum-1) {
-                if(m != i+1){
-                    //отправляет сообщение recv процессу с рангом на 1 больше своего
-                    printf("\n Process sending massage: %3d", recv);
-                    printf("\n Current time is: %3d", MPI_Wtime());
-                    MPI_Send(&recv, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-                }
-            }
-            //любой другой процесс
-            else {
-                //отправляет сообщение recv процессу с рангом на 1 больше своего
-                printf("\n Process %3d sending massage: %3d", recv);
-                printf("\n Current time is: %3d", MPI_Wtime());
-                MPI_Send(&recv, 1, MPI_INT, procRank+1, 0, MPI_COMM_WORLD);
-            }
+        //Получаем сообщение
+        MPI_Bcast(&recv, 1, MPI_INT, fromProc, MPI_COMM_WORLD);
+        cout << "Processor " << procRank << " recv the message " << recv << endl;
+
+        //Увеличиваем значение счётчика на 1
+        int nextCount = recv + 1;
+        cout << "Processor " << procRank << " send the message " << nextCount << endl;
+
+        //Потом отправляем сообщение
+        MPI_Bcast(&nextCount, 1, MPI_INT, procRank, MPI_COMM_WORLD);
+
+        //Проверяем завершение цикла, m кол-во итераций, а (recv-1)/procNum текующая итерация
+        if((recv-1)/procNum == m-1){
+            break;
         }
     }
     //--------------------Завершение работы MPI--------------------//
     MPI_Finalize();
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+
     return 0;
 }
