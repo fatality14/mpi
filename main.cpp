3680bfa733814563aa35dbad46bbc8a678938111
zhenya11114 <fatality14nik@gmail.com>
2021-02-26 17:34:56 +0300

    lab2.1

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 #include <iostream>
 #include <cstring>
 #include <chrono>
+#include <time.h>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
     //--------------------Иициализация--------------------//
+    int sizeX = 100;
+
     int procNum, procRank, recv;
     int m=5;//кол-во итераций
 
@@ -16,6 +19,10 @@ int main(int argc, char* argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
 
+    //семя для функции rand(), смещено, чтобы было разное у разных процессов
+    srand(time(0)+procRank + 314159 * 451 % 42);
+
+    int allSum = 0;
 
     auto start = std::chrono::steady_clock::now();
 
@@ -29,8 +36,21 @@ int main(int argc, char* argv[])
         MPI_Bcast(&recv, 1, MPI_INT, procRank, MPI_COMM_WORLD);
     }
     //--------------------Главный цикл--------------------//
-    while(true)
-    {
+    while(true) {
+        int globalSum = 0;
+        int localSum = 0;
+        for(int i = 0; i < sizeX; ++i){
+            localSum += rand() % 100;
+        }
+
+        cout << "Local sum of process " << procRank << " is " << localSum << endl;
+        MPI_Reduce(&localSum, &globalSum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+        if(procRank == 0){
+            allSum += globalSum;
+            cout << "Global sum is " << globalSum << endl;
+        }
+
         //Вычисляем значение root, из которого будем получать сообщение
         int fromProc = (procNum+procRank-1) % procNum;
         cout << "Processor " << procRank << " recv the message from root " <<  fromProc << endl;
@@ -51,6 +71,11 @@ int main(int argc, char* argv[])
             break;
         }
     }
+
+    if(procRank == 0){
+        cout << "Final sum is " << allSum << endl;
+    }
+
     //--------------------Завершение работы MPI--------------------//
     MPI_Finalize();
 
