7173716c84ae7f4ebd7e7a3296380a51d00602e5
zhenya11114 <fatality14nik@gmail.com>
2021-03-04 13:30:40 +0300

    lab2.2

--- main.cpp
+++ main.cpp
@@ -22,58 +22,27 @@ int main(int argc, char* argv[])
     //семя для функции rand(), смещено, чтобы было разное у разных процессов
     srand(time(0)+procRank + 314159 * 451 % 42);
 
-    int allSum = 0;
-
     auto start = std::chrono::steady_clock::now();
 
     //--------------------Запуск цикла--------------------//
     if (procRank == 0) {
-        recv = 0;
-
         cout << "Num of processors: " << procNum << endl;
-        cout << "Main process with rank: " << procRank << endl;
-        cout << "Start broadcasting" << endl;
-        MPI_Bcast(&recv, 1, MPI_INT, procRank, MPI_COMM_WORLD);
     }
     //--------------------Главный цикл--------------------//
-    while(true) {
-        int globalSum = 0;
-        int localSum = 0;
-        for(int i = 0; i < sizeX; ++i){
-            localSum += rand() % 100;
+    for(int j = 0; j < m; ++j){
+        for(int i = 0; i < procNum; ++i) {
+            int globalSum = 0;
+            int localSum = 0;
+
+            localSum = rand()%100;
+
+            //cout << "Local sum of process " << procRank << " is " << localSum << endl;
+            MPI_Reduce(&localSum, &globalSum, 1, MPI_INT, MPI_SUM, i, MPI_COMM_WORLD);
+
+            if(globalSum != 0){
+                cout << "Process " << procRank << " recieved " << globalSum << endl;
+            }
         }
-
-        cout << "Local sum of process " << procRank << " is " << localSum << endl;
-        MPI_Reduce(&localSum, &globalSum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-
-        if(procRank == 0){
-            allSum += globalSum;
-            cout << "Global sum is " << globalSum << endl;
-        }
-
-        //Вычисляем значение root, из которого будем получать сообщение
-        int fromProc = (procNum+procRank-1) % procNum;
-        cout << "Processor " << procRank << " recv the message from root " <<  fromProc << endl;
-
-        //Получаем сообщение
-        MPI_Bcast(&recv, 1, MPI_INT, fromProc, MPI_COMM_WORLD);
-        cout << "Processor " << procRank << " recv the message " << recv << endl;
-
-        //Увеличиваем значение счётчика на 1
-        int nextCount = recv + 1;
-        cout << "Processor " << procRank << " send the message " << nextCount << endl;
-
-        //Потом отправляем сообщение
-        MPI_Bcast(&nextCount, 1, MPI_INT, procRank, MPI_COMM_WORLD);
-
-        //Проверяем завершение цикла, m кол-во итераций, а (recv-1)/procNum текующая итерация
-        if((recv-1)/procNum == m-1){
-            break;
-        }
-    }
-
-    if(procRank == 0){
-        cout << "Final sum is " << allSum << endl;
     }
 
     //--------------------Завершение работы MPI--------------------//
